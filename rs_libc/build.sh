865c61d4bbdb954e225c9cfc94db972a4e9a4f67
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-07-25 16:14:19 +0300

    Rename example

--- rs_libc/build.sh
+++ rs_libc/build.sh
@@ -6,4 +6,6 @@ set -x
 rm -rf target *.wasm *.wat *.o
 cargo build --target=wasm32-wasi --release
 clang --target=wasm32-wasi main.c -c -o main.o
-emcc main.o target/wasm32-wasi/release/librs_static_lib_libc.a -o main.wasm
+emcc main.o target/wasm32-wasi/release/librs_libc.a -o main.wasm
+wasm2wat main.wasm > main.wat
+wasmtime main.wasm
