5bbff3f6a55e06190ad9a72a5bbe3bd4adf3f3a1
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-07-29 10:56:34 +0300

    Update rs_static_lib example

--- rs_static_lib/build.sh
+++ rs_static_lib/build.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 rm -rf target *.o *.wasm *.wat
-cargo build --target wasm32-wasi --release
-clang --target=wasm32-wasi main.c -fpic -c -o main.o
-wasm-ld main.o target/wasm32-wasi/release/librs_static_lib.a -o main.wasm
+cargo build --target wasm32-unknown-emscripten --release
+clang --target=wasm32-unknown-emscripten main.c -fpic -c -o main.o
+wasm-ld main.o --whole-archive target/wasm32-unknown-emscripten/release/librs_static_lib.a -o main.wasm
