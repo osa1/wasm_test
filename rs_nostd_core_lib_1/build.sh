9daf676f5c68a2254657b952a2495cb64fc8636b
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-07-29 12:03:23 +0300

    Simplify one of the build files

--- rs_nostd_core_lib_1/build.sh
+++ rs_nostd_core_lib_1/build.sh
@@ -12,12 +12,15 @@ clang-10 main.o -Wl--whole-archive target/debug/librs_nostd_core_lib_1.a -o main
 
 echo '##### Building wasm32-unknown-emscripten'
 rm -rf target *.o *.wasm *.wat
-cargo rustc --target=wasm32-unknown-emscripten -v -- -Crelocation-model=pic
+cargo build --target=wasm32-unknown-emscripten
 clang-10 --target=wasm32-unknown-emscripten main.c -c -o main.o
+# No need for --whole-archive below before the archive file as we're generating
+# an executable. If we were to generate a shared library we'd need it to be able
+# to include the symbols in Rust archive in the shared library.
 wasm-ld \
     /home/omer/wasi-sdk-11.0/share/wasi-sysroot/lib/wasm32-wasi/libc.a \
     /home/omer/wasi-sdk-11.0/share/wasi-sysroot/lib/wasm32-wasi/crt1.o \
     main.o \
-    --whole-archive target/wasm32-unknown-emscripten/debug/librs_nostd_core_lib_1.a \
+    target/wasm32-unknown-emscripten/debug/librs_nostd_core_lib_1.a \
     -o main.wasm
 wasmtime main.wasm
