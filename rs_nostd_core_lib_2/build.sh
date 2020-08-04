c28dd691a9b00b7b588ad1d1f48d421185d25964
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-08-04 10:07:10 +0300

    Test returning fun ptr in C code

--- rs_nostd_core_lib_2/build.sh
+++ rs_nostd_core_lib_2/build.sh
@@ -6,21 +6,18 @@ set -x
 # echo '##### Building native'
 # rm -rf target *.o *.wasm *.wat *.so main
 # cargo build
-# clang-10 lib.c -c -o lib.o
-# # NOTE: --whole-archive below is necessary otherwise we don't get any of the
-# # symbols in the Rust library in lib_final.o
-# ld.lld --shared \
+# clang-10 -fPIC lib.c -c -o lib.o
+# ld.lld --shared --export-dynamic-symbol=rust_fn --export-dynamic-symbol=c_fn_2 \
 #     lib.o \
-#     --whole-archive target/debug/librs_nostd_core_lib_1.a \
+#     target/debug/librs_nostd_core_lib_1.a \
 #     -o libfinal.so
-# nm libfinal.so
 # clang-10 main.c -c -o main.o
 # clang-10 main.o libfinal.so -o main -lc -lm
 # LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH ./main
 
 echo '##### Building wasm32-unknown-emscripten'
 rm -rf target *.o *.wasm *.wat *.so main
-clang-10 --target=wasm32-unknown-emscripten lib.c -c -o lib.o
+clang-10 -fPIC --target=wasm32-unknown-emscripten lib.c -c -o lib.o
 wasm2wat --enable-all lib.o > lib.wat
 
 # We don't need -Crelocation-model=pic here, probably because it's passed by
@@ -30,10 +27,10 @@ wasm2wat --enable-all lib.o > lib.wat
 #
 # codegen-units=1 is not necessary but it makes it generates less .o files in
 # the final archive and makes it easier to find symbols.
-xargo rustc --target=wasm32-unknown-emscripten -v --release -- \
-    -Crelocation-model=pic -Ccodegen-units=1
+cargo +stage1 rustc --target=wasm32-unknown-emscripten -v --release -- \
+    -Crelocation-model=pic -Ccodegen-units=1 --emit=llvm-ir --emit=mir
 
-wasm-ld --shared --export rust_fn --export c_fn --gc-sections \
+wasm-ld --shared --export rust_fn --export c_fn --export c_fn_2 --gc-sections \
     lib.o \
     target/wasm32-unknown-emscripten/release/librs_nostd_core_lib_1.a \
     -o libfinal.wasm
--- rs_nostd_core_lib_2/lib.c
+++ rs_nostd_core_lib_2/lib.c
@@ -2,3 +2,8 @@ int c_fn(int x, int y)
 {
     return x + y;
 }
+
+int (*c_fn_2(void)) (int x, int y)
+{
+    return &c_fn;
+}
